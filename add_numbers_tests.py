3e9c7520834749a03b6a42230bcde98f2f8bd01e
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-03-22 14:08:49 +0530

    added few more test cases for sum of upto two numbers

--- add_numbers_tests.py
+++ add_numbers_tests.py
@@ -12,3 +12,21 @@ class AddNumbersTests(unittest.TestCase):
 
     def test_returns_sum_of_two_numbers(self):
         self.assertEqual(add("1,2"), 3)
+
+    # couple of edge edge cases
+    def test_returns_sum_of_two_chars(self):
+        self.assertEqual(add("a, b"), 0)
+    
+    def test_returns_sum_of_no_chars(self):
+        self.assertEqual(add(","), 0)
+    
+    # This can be handled to ignore chars but not the requirement
+    def test_returns_sum_of_two_chars_and_numbers(self):
+        self.assertEqual(add("a1, b2"), 0)
+    
+    def test_returns_sum_of_numbers_with_spaces_in_string(self):
+        self.assertEqual(add("1, 2"), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
