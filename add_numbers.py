381ad886988f8f635e03dbf9351e1d212493ce54
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-03-22 12:51:45 +0530

    added tests and code to add up to two numbers in a string

--- add_numbers.py
+++ add_numbers.py
@@ -1,2 +1,9 @@
 def add(numbers):
-    pass
+    if not numbers:
+        return 0
+    numbers = numbers.split(',')
+    sum = 0
+    for i in numbers:
+        if i.isdigit():
+            sum += int(i)
+    return sum
--- add_numbers_tests.py
+++ add_numbers_tests.py
@@ -3,3 +3,12 @@ from add_numbers import (
         add,
     )
 
+class AddNumbersTests(unittest.TestCase):
+    def test_returns_zero_for_empty_string(self):
+        self.assertEqual(add(""), 0)
+    
+    def test_returns_sum_of_one_number(self):
+        self.assertEqual(add("12"), 12)
+
+    def test_returns_sum_of_two_numbers(self):
+        self.assertEqual(add("1,2"), 3)
